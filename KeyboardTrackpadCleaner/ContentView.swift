9dd6e5a73d241e62db5a5d1375c3c21488e149bc
Junian Triajianto <author@junian.dev>
2026-01-22 15:55:34 +0700

    Initial Commit

---
+++ KeyboardTrackpadCleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeyboardTrackpadCleaner
+//
+//  Created by Junian Triajianto on 2026-01-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeyboardTrackpadCleaner/KeyboardTrackpadCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardTrackpadCleanerApp.swift
+//  KeyboardTrackpadCleaner
+//
+//  Created by Junian Triajianto on 2026-01-22.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardTrackpadCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
